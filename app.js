5802a411eecef39428f0a44d1ba99616bb0491e0
Anthony Ponson <antonyponson@gmail.com>
2023-03-05 14:50:20 +0530

    calculator finishedd

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ let buttons = Array.from(document.getElementsByClassName("button"))
 buttons.map((button) => {
     button.addEventListener("click", (e) => {
         switch (e.target.innerText) {
-                case "AC":
+            case "AC":
                 display.innerText = ''
                 break;
 
-                case '←':
-                    display.innerText = display.innerText.slice(0, -1)
-                    break;
+            case '←':
+                display.innerText = display.innerText.slice(0, -1)
+                break;
 
-                default:
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText)
+                } catch {
+                    display.innerText = 'Error'
+                }
+
+                break;
+
+            default:
                 display.innerText += e.target.innerText
-                
+
         }
     });
 });
